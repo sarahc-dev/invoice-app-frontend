2969d67ed13c83caf3b88d762f7d89e832a9a059
sarahc-dev <sarahclements@hotmail.co.uk>
2023-11-06 01:46:09 +0000

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,21 @@
 import { useState } from "react";
 import data from "@/data.json";
 import InvoiceCountStatus from "@/components/invoice/InvoiceCountStatus";
+import NewButton from "@/components/ui/NewButton";
 
 export default function Home() {
   const [invoices, setInvoices] = useState(data);
   return (
-    <main className="px-6 py-8 md:px-12 md:py-15 xl:py-20">
-      <div className="mx-auto max-w-base">
-        <div>
+    <main className="w-full px-6 py-8 md:px-12 md:py-15 xl:py-20">
+      <div className="mx-auto flex max-w-base">
+        <div className="mr-auto">
           <h1 className="heading-md md:heading-lg mb-1.5">Invoices</h1>
           {invoices && (
             <InvoiceCountStatus count={invoices.length} filter="None" />
           )}
         </div>
-        <div></div>
-        <div></div>
+        <div>Filter</div>
+        <NewButton onClick={() => {}} />
       </div>
     </main>
   );
